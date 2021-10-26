eae98a7d9d1b2e51fa569fa57230bc0e3e0b202d
Ayush Gupta <iayushgupta007@gmail.com>
2021-10-26 18:20:28 +0530

    comit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyInheritance
+{
+
+     public class Employee {
+
+         public static int Calc(int num){
+
+             num += 10000;
+
+             return num;
+
+         }
+
+         virtual public void myBase(){
+
+             Console.WriteLine("Employee Base");
+
+         }
+
+        }
+    
+
+    public class SDE : Employee{
+
+        static int baseSal = 50000;
+
+        public int mySal = Calc(baseSal);
+
+        public void myBase(){
+
+            //base.myBase();
+
+            Console.WriteLine("SDE Base");
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SDE s = new SDE();
+
+            Console.WriteLine(s.mySal); 
+
+            s.myBase();           
+        }
+    }
+
+}
+
