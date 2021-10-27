d122cb35f26334562494f5e64a2d003b0a4f6438
Ayush Gupta <iayushgupta007@gmail.com>
2021-10-27 12:06:15 +0530

    git push

--- Program.cs
+++ Program.cs
@@ -2,10 +2,86 @@
 
 namespace MyInheritance
 {
+     
+     interface WhoAmI{ 
+     
+        string name;
+        void Me();
+     }
+     
+     public class EmployeeType : WhoAmI{ 
+       
+       string name; 
 
-     public class Employee {
+       public void Me()
+        {
+            Console.WriteLine($"***{name}***");
+        }
 
-         public static int Calc(int num){
+     }
+     
+     interface EmpAcc
+    {
+
+        int UId;
+        int AccountNumbers;
+        void AccountDetails();
+    }
+
+     public class EmployeeAccount : EmpAcc{ 
+
+        public int UId;
+
+        public int AccountNumber;
+
+        public void TakeDetails(){ 
+           
+            UId = Console.ReadLine();
+
+            AccountNumber = Console.ReadLine();
+        
+        }
+
+
+        public void AccountDetails()
+        {
+
+            Console.WriteLine($"User ID: {UId}");
+            Console.WriteLine($"Acc No.: {AccountNumber}");
+        }
+
+
+      }
+     
+
+     interface EmpRole{
+
+        string ERole;
+        string ETeam;
+        void EmployeeTypeDetails();
+    }
+     
+     public class EmployeeRole : EmpRole{
+
+        public string ERole;
+
+        public string ETeam;
+
+        public void EmployeeTypeDetails()
+        {
+
+            Console.WriteLine($"Emp Role : {ERole}");
+            Console.WriteLine($"Emp Team : {ETeam}");
+        }
+
+     }
+
+     public class Employee : EmployeeAccount, EmployeeRole , EmployeeType {
+
+         
+         public static int bonus = 1000;
+
+         public static int SalaryCalculation(int num){
 
              num += 10000;
 
@@ -13,28 +89,20 @@ namespace MyInheritance
 
          }
 
-         virtual public void myBase(){
-
-             Console.WriteLine("Employee Base");
-
-         }
-
-        }
+     }
     
 
-    public class SDE : Employee{
+    public class Developer : Employee{
 
-        static int baseSal = 50000;
+        public int DeveloperBaseSalary = 50000;
 
-        public int mySal = Calc(baseSal);
+        //public int FinalSalary = Calc(DeveloperBaseSalary);
 
-        public void myBase(){
+    }
 
-            //base.myBase();
+    public class HR : Employee{
 
-            Console.WriteLine("SDE Base");
-
-        }
+        public int baseSal = 40000;
 
 
     }
@@ -43,11 +111,9 @@ namespace MyInheritance
         static void Main(string[] args)
         {
 
-            SDE s = new SDE();
-
-            Console.WriteLine(s.mySal); 
-
-            s.myBase();           
+            Developer dev = new Developer();
+            
+            string name = dev.name;
         }
     }
 
